5af2f72f05540e5dbda500fb81947886332d0179
EC2 Default User <ec2-user@ip-172-31-46-78.ec2.internal>
2022-04-20 00:25:53 +0000

    finished VPC terraform module

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,3 +1,56 @@
+locals {
+  subnets = {
+    "sn-reserved-A"= {
+      cidr_block="10.16.0.0/20"
+      az="us-east-1a"
+    }
+    "sn-db-A"= {
+      cidr_block="10.16.16.0/20"
+      az="us-east-1a"
+    }
+    "sn-app-A"= {
+      cidr_block="10.16.32.0/20"
+      az="us-east-1a"
+    }
+    "sn-web-A"= {
+      cidr_block="10.16.48.0/20"
+      az="us-east-1a"
+    }
+    "sn-reserved-B"= {
+      cidr_block="10.16.64.0/20"
+      az="us-east-1b"
+    }
+    "sn-db-B"= {
+      cidr_block="10.16.80.0/20"
+      az="us-east-1b"
+    }
+    "sn-app-B"= {
+      cidr_block="10.16.96.0/20"
+      az="us-east-1b"
+    }
+    "sn-web-B"= {
+      cidr_block="10.16.112.0/20"
+      az="us-east-1b"
+    }
+    "sn-reserved-C"= {
+      cidr_block="10.16.128.0/20"
+      az="us-east-1c"
+    }
+    "sn-db-C"= {
+      cidr_block="10.16.144.0/20"
+      az="us-east-1c"
+    }
+    "sn-app-C"= {
+      cidr_block="10.16.160.0/20"
+      az="us-east-1c"
+    }
+    "sn-web-C"= {
+      cidr_block="10.16.176.0/20"
+      az="us-east-1c"
+    }
+  }
+}
+
 resource "aws_vpc" "upb_vpc" {
   cidr_block = "10.16.0.0/16"
   assign_generated_ipv6_cidr_block=true
@@ -6,118 +59,43 @@ resource "aws_vpc" "upb_vpc" {
       Name="upb_vpc"
   }
 }
-resource "aws_subnet" "sn_reserved_A" {
+resource "aws_subnet" "subnets" {
+  for_each   = local.subnets
   vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.0.0/20"
-  availability_zone="us-east-1a"
+  cidr_block = each.value.cidr_block
+  availability_zone= each.value.az
 
   tags = {
-    Name = "sn-reserved-A"
-  }
-}
-resource "aws_subnet" "sn_db_A" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.16.0/20"
-  availability_zone="us-east-1a"
-
-  tags = {
-    Name = "sn-db-A"
-  }
-}
-resource "aws_subnet" "sn_app_A" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.32.0/20"
-  availability_zone="us-east-1a"
-
-  tags = {
-    Name = "sn-app-A"
-  }
-}
-resource "aws_subnet" "sn_web_A" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.48.0/20"
-  availability_zone="us-east-1a"
-
-  tags = {
-    Name = "sn-web-A"
+    Name = each.key
   }
 }
 
-
-
-resource "aws_subnet" "sn_reserved_B" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.64.0/20"
-  availability_zone="us-east-1b"
+resource "aws_internet_gateway" "upb_igw" {
+  vpc_id = aws_vpc.upb_vpc.id
 
   tags = {
-    Name = "sn-reserved-B"
-  }
-}
-resource "aws_subnet" "sn_db_B" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.80.0/20"
-  availability_zone="us-east-1b"
-
-  tags = {
-    Name = "sn-db-B"
-  }
-}
-resource "aws_subnet" "sn_app_B" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.96.0/20"
-  availability_zone="us-east-1b"
-
-  tags = {
-    Name = "sn-app-B"
-  }
-}
-resource "aws_subnet" "sn_web_B" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.112.0/20"
-  availability_zone="us-east-1b"
-
-  tags = {
-    Name = "sn-web-B"
+    Name = "upb-igw"
   }
 }
 
+resource "aws_route_table" "upb_rt" {
+  vpc_id = aws_vpc.upb_vpc.id
 
-
-
-resource "aws_subnet" "sn_reserved_C" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.128.0/20"
-  availability_zone="us-east-1c"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.upb_igw.id
+  }
 
   tags = {
-    Name = "sn-reserved-C"
+    Name = "upb-web-rt"
   }
 }
-resource "aws_subnet" "sn_db_C" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.144.0/20"
-  availability_zone="us-east-1c"
-
-  tags = {
-    Name = "sn-db-C"
+resource "aws_route_table_association" "web_rt_association" {
+  for_each = {
+    "sn-web-A" = local.subnets.sn-web-A
+    "sn-web-B" = local.subnets.sn-web-B
+    "sn-web-C" = local.subnets.sn-web-C
   }
-}
-resource "aws_subnet" "sn_app_C" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.160.0/20"
-  availability_zone="us-east-1c"
-
-  tags = {
-    Name = "sn-app-C"
-  }
-}
-resource "aws_subnet" "sn_web_C" {
-  vpc_id     = aws_vpc.upb_vpc.id
-  cidr_block = "10.16.176.0/20"
-  availability_zone="us-east-1c"
-
-  tags = {
-    Name = "sn-web-C"
-  }
+  subnet_id      = aws_subnet.subnets["${each.key}"].id
+  route_table_id = aws_route_table.upb_rt.id
 }
